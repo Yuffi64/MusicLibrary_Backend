faf1be123a013289d8c9770985539e44e70a2a87
Yuffi64 <100430798+Yuffi64@users.noreply.github.com>
2022-04-07 16:55:18 -0500

    created GET and PUT and returns 201 status code

--- songs/serializers.py
+++ songs/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Song
 
 
-class SongSerializer(serializers.ModelSerializers):
+class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
         fields = ['id', 'title', 'artist', 'album', 'release_date', 'genre']
