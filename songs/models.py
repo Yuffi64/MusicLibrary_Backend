81d6650f84296a5acd6d982b9cadf38be977b38f
Yuffi64 <100430798+Yuffi64@users.noreply.github.com>
2022-04-07 13:49:32 -0500

    created libraries app. + created song app. and migrated it to mysql

---
+++ songs/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=150)
+    artist = models.CharField(max_length=150)
+    album = models.CharField(max_length=150)
+    release_date = models.DateField()
+    genre = models.CharField(max_length=150)
