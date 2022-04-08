c31e0124dfd11e883b4ba5164c8910ccf3510d74
Yuffi64 <100430798+Yuffi64@users.noreply.github.com>
2022-04-08 11:33:51 -0500

    completed adding the get,post,put and delete api.

--- songs/urls.py
+++ songs/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.music_list)
+    path('', views.music_list),
+    path('<int:pk>/', views.music_detail),
     
 ]
